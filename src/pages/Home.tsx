ee190d45424961b21d823ce3eb31177bca7f00bb
bruno <brunoqueiroz612@gmail.com>
2023-02-28 15:53:11 -0300

    Create initial HTML for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import TierListCard from "../components/TierListCard";
 
 const Home = () => {
-  return <main></main>;
+  return (
+    <main className="flex flex-col items-center gap-8 p-4">
+      <h1 className="text-5xl font-bold text-center mt-4">Your Tier Lists</h1>
+      <a
+        href=""
+        className="py-3 px-4 rounded bg-primary-light-red w-[max-content]"
+      >
+        Create Tier List
+      </a>
+      <div className="flex gap-4 flex-wrap justify-center">
+        <TierListCard />
+        <TierListCard />
+        <TierListCard />
+        <TierListCard />
+        <TierListCard />
+        <TierListCard />
+        <TierListCard />
+        <TierListCard />
+      </div>
+    </main>
+  );
 };
 
 export default Home;
