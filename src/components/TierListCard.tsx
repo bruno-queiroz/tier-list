35d271d9023a02d5494d52ee130b94a94c2b39fb
bruno <brunoqueiroz612@gmail.com>
2023-02-28 15:48:44 -0300

    Create TierListCard component

---
+++ src/components/TierListCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TierListCard = () => {
+  return (
+    <a href="">
+      <article className="flex flex-col gap-4 bg-gray-800 p-3 rounded">
+        <img src="" alt="" className="w-[200px] h-[150px] bg-blue-500" />
+        <span>tierlist name</span>
+      </article>
+    </a>
+  );
+};
+
+export default TierListCard;
