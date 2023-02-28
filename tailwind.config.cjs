f89f6f1eea33cf2f9219d18d720658a0ae5d9fe1
bruno <brunoqueiroz612@gmail.com>
2023-02-28 15:49:57 -0300

    Add primary-light-red color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-light-red": "#FF7F7F",
+      },
+    },
   },
   plugins: [],
 };
