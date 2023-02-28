561356c91330ab6265d56f2edf9352860d0d431e
bruno <brunoqueiroz612@gmail.com>
2023-02-28 16:48:44 -0300

    Implement react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import Header from "./components/Header";
+import CreateTierList from "./pages/CreateTierList";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-tier-list" element={<CreateTierList />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
