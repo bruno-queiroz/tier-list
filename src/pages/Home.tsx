b109b2f961a18eec382e0b7cd80e5d3e1eaac858
bruno <brunoqueiroz612@gmail.com>
2023-03-02 16:43:37 -0300

    Change anchor tag to react-router Link

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import TierListCard from "../components/TierListCard";
 
 const Home = () => {
   return (
     <main className="flex flex-col items-center gap-8 p-4">
       <h1 className="text-5xl font-bold text-center mt-4">Your Tier Lists</h1>
-      <a
-        href=""
-        className="py-3 px-4 rounded bg-primary-light-red w-[max-content]"
+      <Link
+        to="/create-tier-list"
+        className="py-3 px-4 rounded bg-S w-[max-content]"
       >
         Create Tier List
-      </a>
+      </Link>
       <div className="flex gap-4 flex-wrap justify-center">
         <TierListCard />
         <TierListCard />
