9d528dce219ff6583fa8589e4c660b18b2c60c46
bruno <brunoqueiroz612@gmail.com>
2023-03-10 07:07:00 -0300

    Change TierList types

--- src/fetch/getTierLists.ts
+++ src/fetch/getTierLists.ts
@@ -1,7 +1,8 @@
-import { TierList } from "../pages/CreateTierList";
+import { TierListFormData } from "../pages/CreateTierList";
+
+export type TierList = TierListFormData & { _id: string };
 
 export const getTierLists = async () => {
-  console.log("fetching");
   const response = await fetch("http://localhost:3000/get-tier-lists");
   const data: TierList[] = await response.json();
   return data;
