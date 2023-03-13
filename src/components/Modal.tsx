0802fa461a1e623d145a1966aea3acf4e00b0bdb
bruno <brunoqueiroz612@gmail.com>
2023-03-13 08:28:14 -0300

    Add Modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Modal = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex justify-center fixed inset-0 bg-[rgba(0,0,0,0.5)]">
+      <div className="bg-[#1A1A17] rounded p-4 max-w-[800px] w-full mt-8 h-[max-content]">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
