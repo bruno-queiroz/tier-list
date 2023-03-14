f6b45c04f1e2c020ea370905c6f9ce14e0a2e8cc
bruno <brunoqueiroz612@gmail.com>
2023-03-14 08:11:16 -0300

    Add TierListItems to zustand store

--- src/zustandStore/store.ts
+++ src/zustandStore/store.ts
@@ -1,21 +1,25 @@
 import { create } from "zustand";
-import { mockData } from "../../public/mockData";
+import { imagesTest, mockData } from "../../public/mockData";
 import { TierList } from "../pages/TierList";
 
 interface TierListState {
   tierList: TierList[];
+  tierListItems: string[];
   isRowModalOpen: boolean;
   rowModalIndex: number;
   setTierList: (tierList: TierList[]) => void;
+  setTierListItems: (tierListItems: string[]) => void;
   changeRowModalIndex: (index: number) => void;
   changeRowModalState: (trueOrfalse: boolean) => void;
 }
 
 export const useTierListStore = create<TierListState>()((set) => ({
   tierList: mockData,
+  tierListItems: imagesTest,
   isRowModalOpen: false,
   rowModalIndex: 0,
   setTierList: (tierList) => set((state) => ({ tierList })),
+  setTierListItems: (tierListItems) => set((state) => ({ tierListItems })),
   changeRowModalIndex: (index) => set((state) => ({ rowModalIndex: index })),
   changeRowModalState: (trueOrfalse) =>
     set((state) => ({ isRowModalOpen: trueOrfalse })),
