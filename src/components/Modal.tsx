238e327e2abaf933005be88842cd3a6ede66e609
bruno <brunoqueiroz612@gmail.com>
2023-03-23 08:30:28 -0300

    Increase z-index to 50

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Modal = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex justify-center fixed inset-0 bg-[rgba(0,0,0,0.5)]">
+    <div className="flex justify-center fixed inset-0 bg-[rgba(0,0,0,0.5)] z-50">
       <div className="bg-[#1A1A17] rounded p-4 max-w-[800px] w-full mt-8 h-[max-content]">
         {children}
       </div>
