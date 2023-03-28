c97f3da87b963348517f39384b0890f8777e194b
bruno <brunoqueiroz612@gmail.com>
2023-03-28 08:23:00 -0300

    Add link to the logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,9 @@ const Header = () => {
   return (
     <header className="p-4 py-6 w-full bg-[#1A1A17]">
       <div className="flex items-center gap-8">
-        <a href="" className="text-4xl text-[#FFBF7F] font-semibold">
+        <Link to="/" className="text-4xl text-[#FFBF7F] font-semibold">
           TierList Creator
-        </a>
+        </Link>
 
         <nav>
           <ul className="flex gap-4">
