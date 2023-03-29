5b3ca61d8a33c475d16747e9d56001aed331f0a0
bruno <brunoqueiroz612@gmail.com>
2023-03-29 16:16:41 -0300

    Add dragEnterDataTransfer to global state

--- src/zustandStore/store.ts
+++ src/zustandStore/store.ts
@@ -1,15 +1,21 @@
 import { create } from "zustand";
-import { TierList, TierListItem } from "../pages/TierList";
+import {
+  DragEnterDataTransfer,
+  TierList,
+  TierListItem,
+} from "../pages/TierList";
 
 interface TierListState {
   tierList: TierList[];
   tierListItems: TierListItem[];
+  dragEnterDataTransfer: DragEnterDataTransfer;
   isRowModalOpen: boolean;
   isDownloadModalOpen: boolean;
   rowModalIndex: number;
   tierListCanvas: HTMLCanvasElement | null;
   setTierList: (tierList: TierList[]) => void;
   setTierListItems: (tierListItems: TierListItem[]) => void;
+  setDragEnterDataTransfer: (data: DragEnterDataTransfer) => void;
   changeRowModalIndex: (index: number) => void;
   changeRowModalState: (trueOrfalse: boolean) => void;
   changeDownloadModalState: (trueOrfalse: boolean) => void;
@@ -19,12 +25,21 @@ interface TierListState {
 export const useTierListStore = create<TierListState>()((set) => ({
   tierList: [],
   tierListItems: [],
+  dragEnterDataTransfer: {
+    src: "",
+    tierListItemIndex: undefined,
+    dragStartRowIndex: undefined,
+    isItemSelected: false,
+    tierListNotSelectedItemIndex: undefined,
+  },
   isRowModalOpen: false,
   isDownloadModalOpen: false,
   rowModalIndex: 0,
   tierListCanvas: null,
   setTierList: (tierList) => set((state) => ({ tierList })),
   setTierListItems: (tierListItems) => set((state) => ({ tierListItems })),
+  setDragEnterDataTransfer: (data) =>
+    set((state) => ({ dragEnterDataTransfer: data })),
   changeRowModalIndex: (index) => set((state) => ({ rowModalIndex: index })),
   changeRowModalState: (trueOrfalse) =>
     set((state) => ({ isRowModalOpen: trueOrfalse })),
