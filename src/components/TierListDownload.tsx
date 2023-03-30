a0104b62c5385a4344314012cfb3d8e17969b0e3
bruno <brunoqueiroz612@gmail.com>
2023-03-30 08:54:03 -0300

    Create TierListDownload component

---
+++ src/components/TierListDownload.tsx
@@ -0,0 +1,32 @@
+import html2canvas from "html2canvas";
+import React from "react";
+import { useTierListStore } from "../zustandStore/store";
+
+const TierListDownload = ({
+  tierListRef,
+}: {
+  tierListRef: React.RefObject<HTMLDivElement>;
+}) => {
+  const changeDownloadModalState = useTierListStore(
+    (state) => state.changeDownloadModalState
+  );
+  const setTierListCanvas = useTierListStore(
+    (state) => state.setTierListCanvas
+  );
+  const downloadTierlist = async () => {
+    changeDownloadModalState(true);
+    console.log(tierListRef);
+    const canvas = await html2canvas(tierListRef.current as HTMLElement);
+    setTierListCanvas(canvas);
+  };
+  return (
+    <button
+      className="py-2 px-6 rounded bg-S w-[max-content] mx-auto my-4"
+      onClick={downloadTierlist}
+    >
+      Download
+    </button>
+  );
+};
+
+export default TierListDownload;
