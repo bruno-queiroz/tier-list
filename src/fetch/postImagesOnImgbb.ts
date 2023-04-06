1dead6d8804c6ee5888e75a664901d831cc14bff
bruno <brunoqueiroz612@gmail.com>
2023-04-06 15:55:26 -0300

    Create postImagesOnImbb fetch function

---
+++ src/fetch/postImagesOnImgbb.ts
@@ -0,0 +1,54 @@
+import { apiBaseUrl } from "./apiConfig";
+
+export interface ImageData {
+  id: string;
+  title: string;
+  url_viewer: string;
+  url: string;
+  display_url: string;
+  width: string;
+  height: string;
+  size: string;
+  time: string;
+  expiration: string;
+  image: {
+    filename: string;
+    name: string;
+    mime: string;
+    extension: string;
+    url: string;
+  };
+  thumb: {
+    filename: string;
+    name: string;
+    mime: string;
+    extension: string;
+    url: string;
+  };
+  medium: {
+    filename: string;
+    name: string;
+    mime: string;
+    extension: string;
+    url: string;
+  };
+  delete_url: string;
+}
+
+export interface ImageResponse {
+  data: ImageData;
+  success: boolean;
+  status: number;
+}
+
+export const postImagesOnImgbb = async (tierList: FormData) => {
+  const response = await fetch(
+    `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`,
+    {
+      method: "POST",
+      body: tierList,
+    }
+  );
+  const data: ImageResponse = await response.json();
+  return data;
+};
