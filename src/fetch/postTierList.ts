b673df3ae839530bc6cbe9e0312850eb9e07a303
bruno <brunoqueiroz612@gmail.com>
2023-04-06 15:55:57 -0300

    Add headers to postTierList fetch function

--- src/fetch/postTierList.ts
+++ src/fetch/postTierList.ts
@@ -1,9 +1,13 @@
+import { TierListFormData } from "../pages/CreateTierList";
 import { apiBaseUrl } from "./apiConfig";
 
-export const postTierList = async (tierList: FormData) => {
+export const postTierList = async (tierList: TierListFormData) => {
   const response = await fetch(`${apiBaseUrl}/create-tier-list`, {
     method: "POST",
-    body: tierList,
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(tierList),
   });
   const data = await response.json();
   return data;
