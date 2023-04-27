e550181bd9ed80d48bc15c75a8f46fb131cd9f7c
bruno <brunoqueiroz612@gmail.com>
2023-04-27 08:04:18 -0300

    Create Spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div>
+      <div className="w-[20px] h-[20px] rounded-full border-t-[5px] border-r-primaryDarkGray border-r-[5px] border-t-primaryDarkGray border-transparent border-[5px] animate-spin"></div>
+    </div>
+  );
+};
+
+export default Spinner;
