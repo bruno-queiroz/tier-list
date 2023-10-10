9982291af07080b6369f1bf4002caf36ecc60492
bruno <brunoqueiroz612@gmail.com>
2023-10-10 09:42:23 -0300

    Modify TierListResponse

--- src/fetch/getTierLists.ts
+++ src/fetch/getTierLists.ts
@@ -1,10 +1,18 @@
 import { TierListFormData } from "../pages/CreateTierList";
 import { apiBaseUrl } from "./apiConfig";
 
-export type TierListResponse = TierListFormData & { _id: string };
+type TierListData = TierListFormData & {
+  _id: string;
+};
+
+export type TierListResponse = {
+  data: TierListData[];
+  msg: string;
+  isOk: boolean;
+};
 
 export const getTierLists = async () => {
   const response = await fetch(`${apiBaseUrl}/tier-list`);
-  const data: TierListResponse[] = await response.json();
+  const data: TierListResponse = await response.json();
   return data;
 };
