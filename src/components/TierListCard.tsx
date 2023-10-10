aa642ccd3dd401bfa251a4b1800f6ac4e9c213ea
bruno <brunoqueiroz612@gmail.com>
2023-10-10 09:35:09 -0300

    Create TierListCardProps

--- src/components/TierListCard.tsx
+++ src/components/TierListCard.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { TierListResponse } from "../fetch/getTierLists";
+
+interface TierListCardProps {
+  tierListName: string;
+  tierListImage: string;
+  _id: string;
+}
 
 const TierListCard = ({
   tierListName,
   tierListImage,
   _id,
-}: TierListResponse) => {
+}: TierListCardProps) => {
   return (
     <Link to={`tier-list/${_id}`}>
       <article className="flex flex-col gap-4 bg-primaryDarkGray p-3 rounded">
