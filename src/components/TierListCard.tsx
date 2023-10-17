f1ccd976ff8e829ac5b38a10c28edb7583388615
bruno <brunoqueiroz612@gmail.com>
2023-10-17 10:45:34 -0300

    Add testid to TierListCard

--- src/components/TierListCard.tsx
+++ src/components/TierListCard.tsx
@@ -4,15 +4,17 @@ interface TierListCardProps {
   tierListName: string;
   tierListImage: string;
   _id: string;
+  index: number;
 }
 
 const TierListCard = ({
   tierListName,
   tierListImage,
   _id,
+  index,
 }: TierListCardProps) => {
   return (
-    <Link to={`tier-list/${_id}`}>
+    <Link to={`tier-list/${_id}`} data-testid={`tier-list-card-${index}`}>
       <article className="flex flex-col gap-4 bg-primaryDarkGray p-3 rounded">
         <img
           src={tierListImage}
